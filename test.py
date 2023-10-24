501afdca960da5e13a64fed459250efd53bdfabe
Ans Hussain <i201824@nu.edu.pk>
2023-10-24 09:15:33 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ from main import Bulb
 
 bulb = Bulb()
 
+#Test case-1
 def test_bulb_status():
     assert bulb.getStatus() == "Bulb is not glowing"
 
 
+#Test case-2
 def test_bulb_on():
     bulb.isOn()
     assert bulb.getStatus() == "Bulb is glowing"
 
+#Test case-3
 def test_bulb_off():
     bulb.isOff()
     assert bulb.getStatus() == "Bulb is glowing"
